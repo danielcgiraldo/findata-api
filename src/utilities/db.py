56c8b3e6184719f09e7d9b1547fefd429ee2ca45
Daniel Castillo <dc.giraldocg@hotmail.com>
2023-10-16 01:41:53 -0500

    pymysql

--- src/utilities/db.py
+++ src/utilities/db.py
@@ -1,4 +1,4 @@
-import MySQLdb
+import pymysql
 import os
 from dotenv import load_dotenv
 
@@ -9,22 +9,28 @@ load_dotenv()
 class DB:
     def __init__(self):
         try:
-            self.connection = MySQLdb.connect(
+            self.connection = pymysql.connect(
                 host=os.getenv("DATABASE_HOST"),
                 user=os.getenv("DATABASE_USERNAME"),
-                passwd=os.getenv("DATABASE_PASSWORD"),
+                password=os.getenv("DATABASE_PASSWORD"),
                 db=os.getenv("DATABASE"),
                 autocommit=True,
-                ssl_mode="VERIFY_IDENTITY",
-                ssl={"ca": os.getenv("DATABASE_SSL_CA")},
-                wait_timeout=180
+                ssl={
+                    "ca": os.getenv("DATABASE_SSL_CA"),
+                    "cert": os.getenv("DATABASE_SSL_CERT"),
+                    "key": os.getenv("DATABASE_SSL_KEY"),
+                },
+                charset="utf8mb4",
+                cursorclass=pymysql.cursors.DictCursor,
+                connect_timeout=180,
             )
-        except MySQLdb.Error as e:
+        except pymysql.Error as e:
             raise Exception(f"Database connection error: {e}")
+
     def connection(self):
         try:
             self.connection.ping(reconnect=True)
-        except MySQLdb.Error as e:
+        except pymysql.Error as e:
             raise Exception(f"Database connection error: {e}")
 
     def __del__(self):
@@ -39,7 +45,7 @@ class DB:
                 query = f"INSERT INTO {table} ({columns}) VALUES ({placeholders})"
                 cursor.execute(query, list(values.values()))
                 return True
-        except (MySQLdb.Error, Exception) as e:
+        except (pymysql.Error, Exception) as e:
             # Log the error or raise an exception for proper error handling
             print("Error:", e)
             return False
