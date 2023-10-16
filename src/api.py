dcd5297a7eefe8bcf88633f5bcfa8306f6edfb47
Daniel Castillo <dc.giraldocg@hotmail.com>
2023-10-16 01:44:31 -0500

    db insert

--- src/api.py
+++ src/api.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 
+from utilities.db import DB
+
+
+
 app = FastAPI()
 
 @app.get("/")
 async def root():
+    db = DB()
+    db.insert("BANK", {"id": "BC", "name": "Bancolombia", "website": "https://www.grupobancolombia.com", "favicon": "https://www.grupobancolombia.com/favicon.ico"})
     return {"message": "Hello World"}
